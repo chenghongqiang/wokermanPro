e21445528e1d79c902c864a76bf5cb40f27cc663
chenghongqiang <chenghongqiang123@yeah.net>
2018-06-29 18:10:50 +0800

    modify the heartbeat

--- heartbeat.php
+++ heartbeat.php
@@ -13,8 +13,7 @@ use Workerman\Lib\Timer;
 
 define("HEATBEAT_TIME", 30);
 
-$worker = new Worker("Websocket://0.0.0.0:8080");
-$worker->protocol = 'Websocket';
+$worker = new Worker("tcp://0.0.0.0:8080");
 
 $worker->onWorkerStart = function ($worker) {
     Timer::add(1, function() use($worker){
@@ -38,6 +37,12 @@ $worker->onWorkerStart = function ($worker) {
 $worker->onMessage = function ($connection, $msg) {
     $connection->lastMessageTime = time();
 };
+
+$worker->onError = function($connection, $code, $msg)
+{
+    echo "error $code $msg\n";
+};
+
 
 Worker::runAll();
 
