5961f8ab4f8215b03f32917f8bffeabbdc8b868f
chenghongqiang <kewin.cheng@jfz.com>
2018-06-29 16:59:08 +0800

    headbeat

---
+++ heartbeat.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kewin.cheng
+ * Date: 2018/6/29
+ * Time: 16:41
+ */
+
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+use Workerman\Worker;
+use Workerman\Lib\Timer;
+
+define("HEATBEAT_TIME", 30);
+
+$worker = new Worker("Websocket://0.0.0.0:8080");
+$worker->protocol = 'Websocket';
+
+$worker->onWorkerStart = function ($worker) {
+    Timer::add(1, function() use($worker){
+
+        $now = time();
+        foreach($worker->connections as $connection) {
+
+            if(empty($connection->lastMessageTime)) {
+                $connection->lastMessage = $now;
+                continue;
+            }
+
+            if( $now - $connection->lastMessage > HEATBEAT_TIME) {
+                $connection->close();
+            }
+        }
+
+    });
+};
+
+$worker->onMessage = function ($connection, $msg) {
+    $connection->lastMessageTime = time();
+};
+
+Worker::runAll();
+
--- timer.php
+++ timer.php
@@ -4,18 +4,46 @@ use Workerman\Worker;
 
 require_once __DIR__ . '/Workerman/Autoloader.php';
 
-$worker1 = new Worker('tcp://0.0.0.0:8080');
-$worker1->count = 4;
+$worker = new Worker('tcp://0.0.0.0:8080');
+$worker->count = 4;
+$worker->name = 'MyWorkermanPro';
+
+//是否以daemon(守护进程)方式运行
+//Worker::$daemonize = true;
+
 //worker实例1有4个进程，进程id编号将分别为0/1/2/3
-$worker1->onWorkerStart = function($worker1)
+$worker->onWorkerStart = function($worker)
 {
-    echo "worker1->id={$worker1->id}\n";
+    echo "worker->id={$worker->id}\n";
     //只在id编号为0的进程上设置定时器，其他1、2、3号进程不设置定时器
-    if($worker1->id === 0) {
+    if($worker->id === 0) {
         \Workerman\Lib\Timer::add(1, function(){
            echo "4个worker进程，只在0号进程设置定时器\n";
         });
     }
+};
+
+//设置worker收到reload信号后执行的回调
+$worker->onWorkerReload = function ($worker) {
+    foreach ($worker->connections as $connection) {
+        $connection->send("worker reloading");
+    }
+};
+
+$worker->onConnect = function ($connection) {
+    echo "new connection from ip:" . $connection->getRemoteIp() . "\n";
+};
+
+//connection 连接对象，用于操作客户端连接，如发送数据/关闭连接等
+//data 客户端上发来的数据，如果worker指定了协议，则$data是对应协议decode了的数据
+$worker->onMessage = function ($connection, $data) {
+    var_dump($data);
+    $connection->send("receive success");
+};
+
+
+$worker->onClose = function ($connection) {
+    echo "connection closed\n";
 };
 
 // worker实例2有两个进程，进程id编号将分别为0、1
